17f019b326930e93d7b2dcfd96f6a1005b3d106d
Paul Kompfner <kompfner@gmail.com>
2020-06-11 19:13:18 -0400

    Implement HTTP conditional requests for call object bundle using custom cache on iOS

--- src/iOSCallObjectBundleCache.ts
+++ src/iOSCallObjectBundleCache.ts
@@ -5,18 +5,28 @@ const FIELD_BUNDLE_CODE = 'code';
 const FIELD_BUNDLE_LAST_MODIFIED = 'last-modified';
 const FIELD_BUNDLE_ETAG = 'etag';
 
+const REQUIRED_FIELDS = [FIELD_BUNDLE_CODE, FIELD_CACHE_EXPIRY];
+
 const DEFAULT_EXPIRY_MS = 60 * 1000;
 
 function cacheKeyForUrl(url: string): string {
   return `callobj_bundle_${url}`;
 }
 
+type CacheResponse = {
+  code?: string;
+  refetchHeaders?: Headers;
+};
+
 /**
  * A workaround for the fact that the iOS HTTP cache won't cache the call
  * object bundle due to size.
  */
 export default class iOSCallObjectBundleCache {
-  static async get(url: string): Promise<string | null> {
+  static async get(
+    url: string,
+    ignoreExpiry = false
+  ): Promise<CacheResponse | null> {
     // console.log("[iOSCallObjectBundleCache] get", url);
 
     const now = Date.now();
@@ -30,25 +40,32 @@ export default class iOSCallObjectBundleCache {
         return null;
       }
 
-      const cacheItem = JSON.parse(cacheItemString);
+      const cacheItem: { [key: string]: any } = JSON.parse(cacheItemString);
 
       // Check cache item for missing required fields
-      const requiredCacheFields = [FIELD_CACHE_EXPIRY, FIELD_BUNDLE_CODE];
-      for (const field of requiredCacheFields) {
+      for (const field of REQUIRED_FIELDS) {
         if (!cacheItem[field]) {
-          throw new Error(`Missing cache item field: ${field}`);
+          throw new Error(
+            `Missing call object bundle cache item field: ${field}`
+          );
         }
       }
 
-      // If cache expired, clear it and return null
-      if (Date.now() > cacheItem[FIELD_CACHE_EXPIRY]) {
+      // If cache expired, return headers needed to conditionally fetch the
+      // bundle again
+      if (!ignoreExpiry && now > cacheItem[FIELD_CACHE_EXPIRY]) {
         // console.log("[iOSCallObjectBundleCache] cache item expired");
-        AsyncStorage.removeItem(cacheKeyForUrl(url));
-        return null;
+        let headers: { [key: string]: string } = {};
+        cacheItem[FIELD_BUNDLE_ETAG] &&
+          (headers['if-none-match'] = cacheItem[FIELD_BUNDLE_ETAG]);
+        cacheItem[FIELD_BUNDLE_LAST_MODIFIED] &&
+          (headers['if-modified-since'] =
+            cacheItem[FIELD_BUNDLE_LAST_MODIFIED]);
+        return { refetchHeaders: new Headers(headers) };
       }
 
       // Return cached code
-      return cacheItem[FIELD_BUNDLE_CODE];
+      return { code: cacheItem[FIELD_BUNDLE_CODE] };
     } catch (e) {
       // console.log("[iOSCallObjectBundleCache] error in get", url, e);
 
@@ -58,6 +75,22 @@ export default class iOSCallObjectBundleCache {
     }
   }
 
+  static async renew(url: string, headers: Headers): Promise<CacheResponse> {
+    // console.log("[iOSCallObjectBundleCache] renew", url);
+
+    const cacheResponse = await this.get(url, true);
+
+    if (!(cacheResponse && cacheResponse.code)) {
+      throw new Error(
+        'Attempting to renew a call object bundle cache item that is missing'
+      );
+    }
+
+    this.set(url, cacheResponse.code, headers);
+
+    return cacheResponse;
+  }
+
   static async set(url: string, code: string, headers: Headers): Promise<void> {
     // console.log("[iOSCallObjectBundleCache] set", url, headers);
 
@@ -65,19 +98,25 @@ export default class iOSCallObjectBundleCache {
       return;
     }
 
-    // Get cache expiry from cache-control header
+    // Get cache expiry from cache-control header (or use a default value)
     let expiry = DEFAULT_EXPIRY_MS;
     const cacheControlHeader = headers.get('cache-control');
-    if (cacheControlHeader && cacheControlHeader.includes('public')) {
+    if (cacheControlHeader) {
       const expiryString = cacheControlHeader.match(/max-age=([0-9]+)/i);
       if (expiryString && expiryString[1] && Number(expiryString[1])) {
         expiry = Date.now() + Number(expiryString[1]) * 1000;
       }
     }
 
+    // Get validators for conditional requests
+    const etag = headers.get('etag');
+    const lastModified = headers.get('last-modified');
+
     let cacheItem: { [key: string]: any } = {};
     cacheItem[FIELD_BUNDLE_CODE] = code;
     cacheItem[FIELD_CACHE_EXPIRY] = expiry;
+    cacheItem[FIELD_BUNDLE_ETAG] = etag;
+    cacheItem[FIELD_BUNDLE_LAST_MODIFIED] = lastModified;
 
     return AsyncStorage.setItem(cacheKeyForUrl(url), JSON.stringify(cacheItem));
   }
