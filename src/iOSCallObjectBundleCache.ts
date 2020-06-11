7dc5414a2f626e4ccb380cfae01910a7dbc3d0b9
Paul Kompfner <kompfner@gmail.com>
2020-06-11 15:14:30 -0400

    Implement dumb "forever cache" of call object bundles on iOS

--- src/iOSCallObjectBundleCache.ts
+++ src/iOSCallObjectBundleCache.ts
@@ -1,15 +1,17 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
 /**
- * A workaround for the fact that the iOS HTTP cache is not caching the call
+ * A workaround for the fact that the iOS HTTP cache won't cache the call
  * object bundle due to size.
  */
 export default class iOSCallObjectBundleCache {
-  static get(url: string): Promise<string> {
+  static get(url: string): Promise<string | null> {
     // console.log("[iOSCallObjectBundleCache] get", url);
-    return Promise.reject();
+    return AsyncStorage.getItem(`callobj_bundle_${url}`);
   }
 
   static set(url: string, bundle: string): Promise<void> {
     // console.log("[iOSCallObjectBundleCache] set", url);
-    return Promise.reject();
+    return AsyncStorage.setItem(`callobj_bundle_${url}`, bundle);
   }
 }
