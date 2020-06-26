0e1509a545766f6e3268ad865ca7939ce65bb79a
Paul Kompfner <kompfner@gmail.com>
2020-06-25 23:39:26 -0400

    In React Native on Android, prevent screen from automatically dimming/locking while media is played in DailyMediaView

--- android/src/main/java/com/reactlibrary/DailyNativeUtils.java
+++ android/src/main/java/com/reactlibrary/DailyNativeUtils.java
@@ -1,13 +1,23 @@
 package com.reactlibrary;
 
+import android.app.Activity;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class DailyNativeUtils extends ReactContextBaseJavaModule {
 
+    private static final String TAG = DailyNativeUtils.class.getName();
+
     private final ReactApplicationContext reactContext;
+    private Set<String> mediaIdsPlaying = new HashSet<>();
 
     public DailyNativeUtils(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -20,8 +30,33 @@ public class DailyNativeUtils extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public  void registerStartedPlayingMedia(String mediaId) {
+        Activity activity = getCurrentActivity();
+        if (activity != null) {
+            activity.runOnUiThread(() -> {
+                mediaIdsPlaying.add(mediaId);
+                updateKeepScreenOnFlag(activity);
+            });
+        }
+    }
+
+    @ReactMethod
+    public  void registerStoppedPlayingMedia(String mediaId) {
+        Activity activity = getCurrentActivity();
+        if (activity != null) {
+            activity.runOnUiThread(() -> {
+                mediaIdsPlaying.remove(mediaId);
+                updateKeepScreenOnFlag(activity);
+            });
+        }
+    }
+
+    private  void updateKeepScreenOnFlag(Activity activity) {
+        Window window = activity.getWindow();
+        if (mediaIdsPlaying.size() > 0) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        } else {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
     }
 }
