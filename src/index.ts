146bb0842a61beb94b66d5ca8d230067929ee961
Paul Kompfner <kompfner@gmail.com>
2020-08-25 18:59:38 -0400

    Use react-native-background-timer to replace set/clearTimeout/Interval on Android with background-friendly versions, letting the app running properly when in the background

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { registerGlobals } from '@daily-co/react-native-webrtc';
 import DailyMediaView from './DailyMediaView';
 import iOSCallObjectBundleCache from './iOSCallObjectBundleCache';
 import 'react-native-url-polyfill/auto'; // Applies global URL polyfill
+import BackgroundTimer from 'react-native-background-timer';
 import { Platform, NativeModules } from 'react-native';
 const { DailyNativeUtils, WebRTCModule } = NativeModules;
 
@@ -18,6 +19,20 @@ function setupGlobals() {
   // A workaround for iOS HTTP cache not caching call object bundle due to size
   if (Platform.OS === 'ios') {
     global.iOSCallObjectBundleCache = iOSCallObjectBundleCache;
+  }
+
+  // Let timers run while Android app is in the background.
+  // See https://github.com/jitsi/jitsi-meet/blob/caabdadf19ae5def3f8173acec6c49111f50a04e/react/features/mobile/polyfills/browser.js#L409,
+  // where this technique was borrowed from.
+  // For now we don't need this for iOS since we're recommending that apps use
+  // the "voip" background mode capability, which keeps the app running normally
+  // during a call.
+  if (Platform.OS === 'android') {
+    global.clearTimeout = BackgroundTimer.clearTimeout.bind(BackgroundTimer);
+    global.clearInterval = BackgroundTimer.clearInterval.bind(BackgroundTimer);
+    global.setInterval = BackgroundTimer.setInterval.bind(BackgroundTimer);
+    global.setTimeout = (fn: () => void, ms = 0) =>
+      BackgroundTimer.setTimeout(fn, ms);
   }
 
   global.DailyNativeUtils = {
