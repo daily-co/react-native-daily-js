3150c447562326aaa21c15b92c65685ced89aef2
Paul Kompfner <kompfner@gmail.com>
2020-10-06 11:12:39 -0400

    Add enableNoOpRecordingEnsuringBackgroundContinuity method to DailyNativeUtils on iOS, to help ensure that execution will continue when backgrounding the app mid-join, or when backgrounding the app when there is no media playing or being captured in a meeting

--- ios/DailyNativeUtils.m
+++ ios/DailyNativeUtils.m
@@ -1,9 +1,14 @@
 #import "DailyNativeUtils.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface DailyNativeUtils()
 
-// State expects to only be accessed on main thread, for thread safety
+// Expects to only be accessed on main thread
 @property (nonatomic, strong) NSMutableSet *requestersKeepingDeviceAwake;
+
+// Expects to only be accessed on captureSessionQueue
+@property (nonatomic, strong) AVCaptureSession *captureSession;
+@property (nonatomic, strong, readonly) dispatch_queue_t captureSessionQueue;
 
 @end
 
@@ -15,6 +20,7 @@ RCT_EXPORT_MODULE()
 {
   if (self = [super init]) {
     _requestersKeepingDeviceAwake = [NSMutableSet set];
+    _captureSessionQueue = dispatch_queue_create("com.daily.noopcapturesession", DISPATCH_QUEUE_SERIAL);
   }
   return self;
 }
@@ -39,14 +45,24 @@ RCT_EXPORT_METHOD(setKeepDeviceAwake:(BOOL)keepDeviceAwake onBehalfOfRequester:(
   }];
 }
 
-#pragma mark Private
-
-// Expects to be invoked only from main thread
-- (void)updateIdleTimer
-{
-  BOOL disableIdleTimer = _requestersKeepingDeviceAwake.count > 0;
-  [[UIApplication sharedApplication] setIdleTimerDisabled:disableIdleTimer];
+RCT_EXPORT_METHOD(enableNoOpRecordingEnsuringBackgroundContinuity:(BOOL)enable) {
+  dispatch_async(self.captureSessionQueue, ^{
+    if (enable) {
+      if (self.captureSession) {
+        return;
+      }
+      AVCaptureSession *captureSession = [self configuredCaptureSession];
+      [captureSession startRunning];
+      self.captureSession = captureSession;
+    }
+    else {
+      [self.captureSession stopRunning];
+      self.captureSession = nil;
+    }
+  });
 }
+
+#pragma mark Private
 
 + (void)dispatchSyncOnMain:(void (^)(void))block
 {
@@ -55,6 +71,42 @@ RCT_EXPORT_METHOD(setKeepDeviceAwake:(BOOL)keepDeviceAwake onBehalfOfRequester:(
   } else {
     dispatch_sync(dispatch_get_main_queue(), block);
   }
+}
+
+// Expects to be invoked from main thread
+- (void)updateIdleTimer
+{
+  BOOL disableIdleTimer = _requestersKeepingDeviceAwake.count > 0;
+  [[UIApplication sharedApplication] setIdleTimerDisabled:disableIdleTimer];
+}
+
+// Expects to be invoked from captureSessionQueue
+- (AVCaptureSession *)configuredCaptureSession
+{
+  AVCaptureSession *captureSession = [[AVCaptureSession alloc] init];
+  AVCaptureDevice *audioDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeAudio];
+  if (!audioDevice) {
+    return nil;
+  }
+  NSError *inputError;
+  AVCaptureDeviceInput *audioInput = [AVCaptureDeviceInput deviceInputWithDevice:audioDevice error:&inputError];
+  if (inputError) {
+    return nil;
+  }
+  if ([captureSession canAddInput:audioInput]) {
+    [captureSession addInput:audioInput];
+  }
+  else {
+    return nil;
+  }
+  AVCaptureAudioDataOutput *audioOutput = [[AVCaptureAudioDataOutput alloc] init];
+  if ([captureSession canAddOutput:audioOutput]) {
+    [captureSession addOutput:audioOutput];
+  }
+  else {
+    return nil;
+  }
+  return captureSession;
 }
 
 @end
