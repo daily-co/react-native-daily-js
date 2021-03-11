5cbaeb99686e89dc55b3b1710801cc25aed347f9
Paul Kompfner <kompfner@gmail.com>
2021-03-11 12:10:07 -0500

    Add an error code for ejected-from-meeting errors, which requires bootstrapping scaffolding and patterns for error codes in general (and, as a bonus, localized error messages for UI usage). Also does a little bit of tidying up along the way.
    Here were the requirements considered when deciding on the above approach to error codes:
    
    1. Backwards compatibility has to be maintained: customers may have written code checking for existing error messages.
    2. A unique code can be provided for each type of error going forward.
    3. Codes can be incrementally adopted; we don’t promise to provide codes for all types of errors on day one.
    4. There is a place where we can someday include localized UI-friendly messages in case customers are cool with just using a drop-in Daily-suggested message, and without interfering with existing error messages (see requirement 1). This message is explicitly advertised as being localized and intended for UI usage, and not guaranteed to be stable (we can edit it over time if needed).
    5. Those localized messages can be incrementally adopted as well; we don’t have to hold off on filling in error codes because we’re waiting on having fully-localized UI-friendly messages to accompany them.

--- type-overrides/@daily-co/daily-js/index.d.ts
+++ type-overrides/@daily-co/daily-js/index.d.ts
@@ -60,6 +60,8 @@ export type DailyMeetingState =
   | 'left-meeting'
   | 'error';
 
+export type DailyMeetingErrorCode = 'ejected';
+
 export interface DailyParticipantsObject {
   local: DailyParticipant;
   [id: string]: DailyParticipant;
@@ -262,10 +264,19 @@ export interface DailyEventObjectNoPayload {
   >;
 }
 
+export interface DailyEventMeetingErrorObject {
+  action: Extract<DailyEvent, 'error'>;
+  errorMsg: string;
+  error?: {
+    code: DailyMeetingErrorCode;
+    localizedMsg?: string;
+  };
+}
+
 export interface DailyEventErrorObject {
   action: Extract<
     DailyEvent,
-    'load-attempt-failed' | 'live-streaming-error' | 'error'
+    'load-attempt-failed' | 'live-streaming-error' | 'camera-error'
   >;
   errorMsg: string;
 }
@@ -324,6 +335,8 @@ export type DailyEventObject<
   ? DailyEventObjectAppMessage
   : T extends DailyEventObjectNoPayload['action']
   ? DailyEventObjectNoPayload
+  : T extends DailyEventMeetingErrorObject['action']
+  ? DailyEventMeetingErrorObject
   : T extends DailyEventErrorObject['action']
   ? DailyEventErrorObject
   : T extends DailyEventObjectParticipants['action']
