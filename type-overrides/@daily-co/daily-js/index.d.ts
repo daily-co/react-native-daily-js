6eed253f2c71e07d95761bcfaf8e21133a2808ad
Filipi Fuchter <filipi87@gmail.com>
2022-01-31 19:20:50 -0300

    Implementing to be able to pass the getUserMedia constraints when invoking to startCamera or joinConference

--- type-overrides/@daily-co/daily-js/index.d.ts
+++ type-overrides/@daily-co/daily-js/index.d.ts
@@ -9,6 +9,7 @@
 import {
   MediaStreamTrack,
   MediaDeviceInfo,
+  MediaTrackConstraints,
 } from '@daily-co/react-native-webrtc';
 
 /**
@@ -120,6 +121,10 @@ export interface DailyLoadOptions extends DailyCallOptions {
 
 export interface DailyAdvancedConfig {
   experimentalGetUserMediaConstraintsModify?: (constraints: any) => void;
+  getUserMediaConstraints?: {
+    audio?: boolean | MediaTrackConstraints;
+    video?: boolean | MediaTrackConstraints;
+  };
   preferH264ForCam?: boolean;
   h264Profile?: string;
 }
