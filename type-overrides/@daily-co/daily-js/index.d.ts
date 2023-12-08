c7e97847405b02b93133f897084d733b2658b51f
Mark Backman <mark@daily.co>
2023-12-08 13:24:54 -0500

    Update extra type from Map to Record

--- type-overrides/@daily-co/daily-js/index.d.ts
+++ type-overrides/@daily-co/daily-js/index.d.ts
@@ -908,7 +908,7 @@ export interface DailyEventObjectTranscriptionStarted {
   redact?: Array<string> | Array<boolean> | boolean;
   endpointing?: number | boolean;
   punctuate?: boolean;
-  extra?: Map<string, any>;
+  extra?: Record<string, any>;
   startedBy: string;
 }
 
@@ -1189,7 +1189,7 @@ export interface DailyTranscriptionDeepgramOptions {
   redact?: Array<string> | Array<boolean> | boolean;
   endpointing?: number | boolean;
   punctuate?: boolean;
-  extra?: Map<string, any>;
+  extra?: Record<string, any>;
 }
 
 export interface DailyCall {
